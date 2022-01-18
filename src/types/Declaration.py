4d37d3f465a2d7ebc6b5109be9e9dc0d60654356
robscott1 <rdavidscott1@gmail.com>
2022-01-18 09:23:55 -0800

    add typeDeclaration and declaration

---
+++ src/types/Declaration.py
@@ -0,0 +1,6 @@
+class Declaration:
+
+    def __init__(self, lineNum, type, id):
+        self.lineNum = lineNum
+        self.type = type
+        self.id = id
---
+++ src/types/TypeDeclaration.py
@@ -0,0 +1,6 @@
+class TypeDeclaration:
+    
+    def __init__(self, lineNum, id, fields):
+        self.lineNum = lineNum
+        self.id = id
+        self.fields = fields
