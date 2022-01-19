a4699c4364486a7becf87e404c790162a8d4cd83
robscott1 <rdavidscott1@gmail.com>
2022-01-19 08:39:36 -0800

    before the destruction

--- src/TypeChecker.py
+++ src/TypeChecker.py
@@ -7,3 +7,13 @@ class TypeChecker:
         # should prevent mix-up with locals.
         # Can be a TypeDeclaration or Declaration
         self.globals = dict()
+
+    '''
+    Checks to make sure there is not a Type with the same name.
+    Global initialization will have further checks.
+    '''
+    def check_type_decl(self, type_decl):
+        if id in self.types:
+            print(f"Error: Type {type_decl.id} has already been defined.")
+            exit()
+        
