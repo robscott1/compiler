663724eacde9c3f45171b89bb86d12dd9586c555
robscott1 <rdavidscott1@gmail.com>
2022-01-31 15:17:25 -0800

    mile1 done except empty return

--- src/Statements/BlockStatement.py
+++ src/Statements/BlockStatement.py
@@ -1,3 +1,4 @@
+from CompilerError import CompilerError
 from Statements.Statement import Statement
 
 
@@ -20,10 +21,20 @@ class BlockStatement(Statement):
     def generate(cls, func, stmt: dict):
         if "list" not in stmt:
             print("found")
-        statements = list(map(
-            lambda x: func(x), stmt.get("list"))
-        ) if len(stmt.get("list")) != 0 \
-            else cls.empty_block()
+
+        statements = []
+        if len(stmt.get("list")) != 0:
+            try:
+                for s in stmt.get("list"):
+                    statements.append(func(s))
+            except CompilerError as e:
+                print(e.msg)
+                exit()
+
+        # statements = list(map(
+        #     lambda x: func(x), stmt.get("list"))
+        # ) if len(stmt.get("list")) != 0 \
+        #     else cls.empty_block()
         return BlockStatement(-1, statements)
 
     def analyze(self, tc):
