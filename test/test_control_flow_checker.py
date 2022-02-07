f2afa18bdb8da6a4409eee2f566b700a007f310d
robscott1 <rdavidscott1@gmail.com>
2022-02-07 10:21:16 -0800

    add code to codecommit

--- test/test_control_flow_checker.py
+++ test/test_control_flow_checker.py
@@ -1,9 +1,11 @@
 # This test is based on the example from ret.mini
+import pytest as pytest
+
 from ControlFlowNode import ControlFlowNode
 from TypeChecker import TypeChecker
 from conftest import program_to_json
 
-
+@pytest.mark.skip("in progress")
 def test_build_control_flow_check():
     cfn_list = []
     params = program_to_json("../mini/ret.mini")
@@ -13,7 +15,7 @@ def test_build_control_flow_check():
         cfn_list.append(root)
     assert len(cfn_list) == 2
 
-
+@pytest.mark.skip("in progress")
 def test_valid_control_flow():
     params = program_to_json("../mini/ret.mini")
     tc = TypeChecker(params)
